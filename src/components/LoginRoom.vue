5e9d20e5d5736bcda1792eb542ce470039d1c8d3
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2018-01-12 09:07:19 +0700

    add game/play room

--- src/components/LoginRoom.vue
+++ src/components/LoginRoom.vue
@@ -1,12 +1,27 @@
 <template>
-  <div class="ui container">
-    <div class="ui middle aligned equal width stackable grid">
+  <div class="ui fluid container">
+    <div class="ui center aligned middle aligned equal width stackable grid">
       <div class="column">
-        <div class="ui center aligned grid">
-          <button class="ui facebook button" @click="login">
-            <i class="facebook icon"></i>
-            Log in with Facebook
-          </button>
+        <div class="row btm">
+          <div class="column">
+            <span class="title">R</span>
+            <img class="rock" src="../assets/rock-font.png" style="">
+            <span class="title space">CK</span>
+            <span class="title">P</span>
+            <img class="paper" src="../assets/paper-font.png" style="">
+            <span class="title space">PER</span>
+            <span class="title">S</span>
+            <img class="scissors" src="../assets/scissors-font.png" style="">
+            <span class="title">ISSORS</span>
+          </div>
+        </div>
+        <div class="row">
+          <div class="column">
+            <button class="ui facebook button" @click="login">
+              <i class="facebook icon"></i>
+              Log in with Facebook
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -42,6 +57,34 @@
 </script>
 
 <style scoped>
+  .title {
+    font-size: 50px;
+    font-weight: 600;
+  }
+
+  .space {
+    padding-right: 30px;
+  }
+
+  .rock {
+    position: relative;
+    top: 8px;
+  }
+
+  .paper {
+    position: relative;
+    top: 14px;
+  }
+
+  .scissors {
+    position: relative;
+    top: 5px;
+  }
+
+  .btm {
+    padding-bottom: 50px !important;
+  }
+
   .ui.middle.aligned.equal.width.stackable.grid {
     height: 100vh;
   }
